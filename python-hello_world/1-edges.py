4d8b1ee236fd31571e3b8a2dec8f93c4487aad3a
KehindeAramide <kenned2k4@yahoo.com>
2023-07-25 17:47:07 +0100

    Copy - Cut - Paste- task1

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+First_3_letters  = word[:3]
+Last_2_letters = word[-2:]
+Middle_word = word[1:-1]
+print("First 3 letters: {}".format(First_3_letters))
+print("Last 2 letters: {}".format(Last_2_letters))
+print("Middle word: {}".format(Middle_word))
+
