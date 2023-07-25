dc77ec9d6bf53af397633ddda5767a1807319934
KehindeAramide <kenned2k4@yahoo.com>
2023-07-25 23:20:29 +0100

    task2 positive and negative

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,3 +2,10 @@
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
+print(number)
+if number > 0:
+    print("is positive")
+if number == 0:
+    print("is  zero")
+else:
+    print("is negative")
