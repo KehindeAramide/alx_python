629b028cad240e3056fd74b41a00da085d0fb1bf
KehindeAramide <kenned2k4@yahoo.com>
2023-07-30 20:56:29 +0100

    task5

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,14 @@
-for i in range(100):
-    print("Decimal : {:.2f } " .format(i), end=", " if i < 99 else "\n")    
+for i in range(100):    
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
 
+
+
+
+
+
+
+
+
+    
 
 
