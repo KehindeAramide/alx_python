5427f2c8863928bfee00b6dbb4cd624af56ec9a5
KehindeAramide <kenned2k4@yahoo.com>
2023-07-30 21:30:09 +0100

    task4 upd

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,8 +1,3 @@
-#for num in range(99):
-  #  decimal = f"{num:2}"      
-  #  hex = f"{num:02X}"    
-   # print(str(decimal) + "=" + str(hex))
-
 for i in range(99):
     print("{:d} = 0x{:x}".format(i,i))
 
