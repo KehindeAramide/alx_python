06bed37ab652ebe1658254b6b4ffa0c9ffecd2c2
KehindeAramide <kenned2k4@yahoo.com>
2023-07-31 10:17:49 +0100

    tsk3 upd

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -3,8 +3,3 @@ def reverse_string(string):
     for i in string:
        reverse_string = i + reverse_string
     return reverse_string
-print(reverse_string("Hello"))
-print(reverse_string(""))
-print(reverse_string("madam"))
-print(reverse_string("Hello, World!"))
-
