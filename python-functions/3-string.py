2d917e182e9a7fb67677b2cc3f2fdffa64728776
KehindeAramide <kenned2k4@yahoo.com>
2023-07-31 10:11:32 +0100

    task3

---
+++ python-functions/3-string.py
@@ -0,0 +1,9 @@
+def reverse_string(string):
+    reverse_string = ""
+    for i in string:
+       reverse_string = i + reverse_string
+    return reverse_string
+
+
+print(reverse_string("Hsssop"))
+
