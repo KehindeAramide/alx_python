5a2b0f62ce2b5ca631f15370ec89ed3633979f3d
KehindeAramide <kenned2k4@yahoo.com>
2023-08-21 21:40:32 +0100

    task5

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,8 +1,2 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
-
-# Call the function to see the exception
-try:
-    raise_exception_msg()
-except NameError as ne:
-    print(ne)
