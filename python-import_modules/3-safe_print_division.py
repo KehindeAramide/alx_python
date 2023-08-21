13b5abd26103645d6720d34ff2f88ac94a367516
KehindeAramide <kenned2k4@yahoo.com>
2023-08-21 19:24:23 +0100

    task3_22

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,10 +1,11 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("Inside result: {}".format(result))
     except:
         result = None
         print("Inside result: {}".format(result))
+    else:
+        print("inside result: {}".format(result))
     finally:
         print("{:d} / {:d} = {}".format(a, b, result))
     
