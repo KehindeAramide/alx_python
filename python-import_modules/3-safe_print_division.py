c07c9fde193b366c333685ab2f4b9c0a8b1fff02
KehindeAramide <kenned2k4@yahoo.com>
2023-08-21 21:53:06 +0100

    task3 new

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,14 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except:
+        result = None
+        #print("Inside result: {}".format(result))
+        #print("Inside result: {}".format(result))
+    else:
+        #print("Inside result: {}".format(result))
+        result = result
+    finally:
+        if 'result' in locals():
+            print("Inside result: {}".format(result))
+            print("{:d} / {:d} = {}".format(a, b, result))
