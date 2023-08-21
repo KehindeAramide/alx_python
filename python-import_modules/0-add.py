8baf9dcd74dff5b1836807df79f492c4a5627920
KehindeAramide <kenned2k4@yahoo.com>
2023-08-21 13:00:48 +0100

    task0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,11 @@
 #import add function from mypackage
 from mypackage.add_0 import add
-a = 1
-b = 2
-#perform the addition using format style
-print("{} + {} = {}" .format(a, b, add(a, b)))
+
+def main():
+    a = 1
+    b = 2
+    # Perform the addition using format style
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    main()
