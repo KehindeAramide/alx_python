88a161455f3130a5d27828d2109388203ff560ea
KehindeAramide <kenned2k4@yahoo.com>
2023-08-21 15:23:51 +0100

    task1 upd

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,11 +5,11 @@ def main():
     num_args = len(argv)
 
     if num_args == 0:
-        print({num_args}, "arguments.")
+        print(num_args, "arguments.")
     elif num_args == 1:
-        print({num_args}, "argument:")
+        print(num_args, "argument:")
     else:
-        print({num_args}, "arguments:")
+        print(num_args, "arguments:")
     for i, arg in enumerate(argv, start=1):
         print("{}: {}".format(i, arg))
     
