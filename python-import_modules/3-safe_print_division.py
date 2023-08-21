ec1308db6949dae46f1af951ed078167f7e42a59
KehindeAramide <kenned2k4@yahoo.com>
2023-08-21 22:44:57 +0100

    task3 new_final

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,8 +3,6 @@ def safe_print_division(a, b):
         result = a / b
     except:
         result = None
-        #print("Inside result: {}".format(result))
-        #print("Inside result: {}".format(result))
     finally:
-        #if 'result' in locals():
         print("Inside result: {}".format(result))
+        return result
