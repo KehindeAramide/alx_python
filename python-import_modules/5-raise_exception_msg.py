e3c8225e1d7f01b1a4cda123b6fa11d77f2a7212
KehindeAramide <kenned2k4@yahoo.com>
2023-08-21 20:56:55 +0100

    task5 upd

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -3,6 +3,6 @@ def raise_exception_msg(message=""):
 
 # Call the function to see the exception
 try:
-    raise_exception_msg("This is a custom NameError message.")
+    raise_exception_msg("C is fun")
 except NameError as ne:
     print(ne)
