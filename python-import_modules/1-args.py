d16b580fbe866f9ee5563888c6e2ef3a45ecaa7b
KehindeAramide <kenned2k4@yahoo.com>
2023-08-21 15:11:13 +0100

    task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,35 +1,17 @@
 import sys
 
 def main():
-    arguments = sys.argv[1:]
-    num_arguments = len(arguments)
+    argv = sys.argv[1:]
+    num_args = len(argv)
 
-    print("Number of argument{}: {}{}".format(
-        's' if num_arguments != 1 else '', num_arguments,
-        '' if num_arguments == 0 else ':'))
-
-    if num_arguments > 0:
-        for i, arg in enumerate(arguments, start=1):
-            print("{}: {}".format(i, arg))
-
+    if num_args == 0:
+        print ("Number of arg(s) .")
+    elif num_args == 1:
+        print("Number of argument(s) argument:")
+    else:
+        print("Number of argument(s) arguments:")
+    for i, arg in enumerate(argv, start=1):
+        print("{}: {}".format(i, arg))
+    
 if __name__ == "__main__":
     main()
-
-import sys
-
-# The number of arguments passed 
-num_args = len(sys.argv) - 1
-
-# Print the number of arguments
-print(f"Number of argument(s): {num_args}")
-
-if num_args == 0:
-    print(".")
-else:
-    # Print "argument" if there's only one argument, otherwise "arguments"
-    plural = "argument" if num_args == 1 else "arguments"
-    print(f"{plural}:")
-
-    # Print each argument along with its position
-    for i in range(1, len(sys.argv)):
-        print(f"{i}: {sys.argv[i]}")
