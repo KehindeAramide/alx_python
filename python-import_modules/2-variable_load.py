c597071fa3bf95979d261cc5ef9c1e3f47a7f691
KehindeAramide <kenned2k4@yahoo.com>
2023-08-21 16:08:16 +0100

    task2 upd

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,8 +1,6 @@
-# importing from variable_load_2
-from variable_load_2 import var
-
 # Print the value of the variable 'a'
 def main():
+    from variable_load_2 import var
     a = 89
     print(var(a))
 
