061ad5566a459e58708a1cd280dabf9710e4b99c
KehindeAramide <kenned2k4@yahoo.com>
2023-08-21 21:11:07 +0100

    task5

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -3,6 +3,6 @@ def raise_exception_msg(message=""):
 
 # Call the function to see the exception
 try:
-    raise_exception_msg("")
+    raise_exception_msg("C is fin")
 except NameError as ne:
     print(ne)
