b0f8088347d4a43e5716ab1cd604c4994e08cde0
KehindeAramide <kenned2k4@yahoo.com>
2023-08-22 15:34:21 +0100

    task 0

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,11 +1,19 @@
 def no_c(my_string):
     new_string = " "
     for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string = char
+        if char not in ('c', 'C'):
+            new_string += char
         return new_string
-    
+        
+        
+        
+        
+        
+        
+        #if char != 'c' and char != 'C':
+            #new_string = char
+    #return new_string
 
-#print(no_c("Holberton School"))
+
 #print(no_c("Chicago"))
-#print(no_c("C is fun!"))
+print(no_c("C is fun!"))
