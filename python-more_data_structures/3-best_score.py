dd312c0fc7f57fc0241d79e4beb8eed0bb51c0cc
KehindeAramide <kenned2k4@yahoo.com>
2023-08-26 14:54:48 +0100

    task3

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,5 +1,15 @@
 def best_score(a_dictionary):
-    biggest_integer = ()
-    for score in a_dictionary:
-        biggest_integer.append(score)
-    return biggest_integer 
+    biggest_int = None
+    biggest_value = float('-inf')
+
+    for int  , value in a_dictionary.items():
+        if value > biggest_value:
+            biggest_int = int
+            biggest_value = value
+    return biggest_int
+
+
+
+    #for score in a_dictionary:
+        #biggest_integer.append(score)
+    #return biggest_integer 
