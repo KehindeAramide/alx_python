e035315116bc62e40f944e7fc2ce528b4ca90dee
KehindeAramide <kenned2k4@yahoo.com>
2023-08-29 13:41:33 +0100

    task0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,9 +2,6 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-    def get_size(self):
-        return self.__size
+    
 
-    #def __str__(self):
-        #return f"Square with side length {self.__size}"
     
