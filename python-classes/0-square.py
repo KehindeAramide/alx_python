bf5be89358a9ee12c68eadd6799d739647fd61ed
KehindeAramide <kenned2k4@yahoo.com>
2023-08-29 12:57:25 +0100

    Task0

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"Square with side length {self.__size}"
