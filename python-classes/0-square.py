fbd7b44e0183432b527f7efd2424f32e17f32ad3
KehindeAramide <kenned2k4@yahoo.com>
2023-08-29 13:56:30 +0100

    task0 upd

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,12 @@
+"""Defining a class square."""
+
 class Square:
-    def __init__(self, size):
-        self.__size = size
+    """Represents a square."""
+def __init__(self, size):
+    """Initializing a new square"""
+    #size(int):
+    """the size of the new square"""
+    self.__size = size
 
     
 
