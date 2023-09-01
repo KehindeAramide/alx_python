1e107d3a81648f06be7601549046ed1368eeb6e5
KehindeAramide <kenned2k4@yahoo.com>
2023-09-01 22:04:47 +0100

    task4

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,5 +1,3 @@
-#def multiply_list_map(my_list=[], number=0):
-    #return (list(map((lambda i: i * number), my_list)))
 def multiply_list_map(my_list=[], number=0):
     multiply = (lambda x: x * number)
     return (list(map(multiply, my_list)))
