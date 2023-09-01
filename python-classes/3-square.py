9cf8034461fb5fd5d8be72bac6fbb53abc2c505a
KehindeAramide <kenned2k4@yahoo.com>
2023-09-01 22:13:08 +0100

    task3 upd

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+"""Defining a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializing a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Returns the current area"""
+        return(self.__size * self.__size)
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
