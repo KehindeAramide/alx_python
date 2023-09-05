c41d88248b80a809824c8dbd15bde9eb42553a35
KehindeAramide <kenned2k4@yahoo.com>
2023-09-05 10:24:44 +0100

    task1

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,14 @@
+"""Defining a class function."""
+def is_kind_of_class(obj, a_class):
+    """Examining if the object is exactly an instance of the specified class.
+    obj(any): the checking obj.
+    a_class(type): obj to be match to this class
+    
+    Returns:
+        True - if object is exactly an instance.
+        otherwise - False.
+    """
+    if isinstance(obj,a_class):
+        return True
+    if type(obj) != a_class:
+        return False
