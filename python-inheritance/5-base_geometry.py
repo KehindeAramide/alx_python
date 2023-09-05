63564f1f8ea81f993c70feb3e84cf371ffd6bf96
KehindeAramide <kenned2k4@yahoo.com>
2023-09-05 19:58:51 +0100

    Task 5 upd

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -2,17 +2,18 @@
 
 
 class BaseGeometry:
-    """This Represents base geometry."""
+    """Reprsent base geometry."""
 
     def area(self):
-        """Not implemented."""
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Validate a parameter as an integer.
-    
+        Args:
             name (str): The name of the parameter.
             value (int): The parameter to validate.
-        
+        Raises:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
