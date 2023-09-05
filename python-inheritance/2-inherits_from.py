c49bb428a1f99877cc88187ac31490220c9227c8
KehindeAramide <kenned2k4@yahoo.com>
2023-09-05 11:15:51 +0100

    task2 upd

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,16 +1,14 @@
-"""Defines an inherited class-checking function."""
-
-
+"""Defining a class function."""
 def inherits_from(obj, a_class):
-    """Checks if an object is an inherited instance of a class.
-
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
+    """Examining if the object is an inherited instance of a_class.
+    obj(any): the checking obj.
+    a_class(type): obj to be match to this class
+    
     Returns:
-        If obj is an inherited instance of a_class - True.
-        Otherwise - False.
+        True - if object is an inherited instance of a_class.
+        otherwise - False.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
+    #if issubclass(type(obj), a_class) and type(obj) == a_class:
     return False
