4dc20623334b91a230458410c5c8eb059e85682e
KehindeAramide <kenned2k4@yahoo.com>
2023-09-06 10:57:09 +0100

    Task 6 upd

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -6,8 +6,14 @@ class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
-    
+        """Intialize a new Rectangle."""
+        bg = BaseGeometry()
+        """Creating an instance for BaseGeometry"""
+        attributes_and_methods = dir(Rectangle)
+        """Getting attributes and methods of instance"""
+        class_attributes_and_methods = dir(Rectangle)
+
+        """
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
         """
@@ -16,3 +22,4 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__height = height
         """creating a rectangle """
+    
