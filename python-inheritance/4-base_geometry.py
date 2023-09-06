3245cc1f3912a1f86da1590db0c230dae3ff2580
KehindeAramide <kenned2k4@yahoo.com>
2023-09-06 09:40:31 +0100

    Task4 upd

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,9 +1,42 @@
-"""Defines a base geometry class BaseGeometry."""
-
-
+"""
+Define an empty class named BaseGeometry
+"""
 class BaseGeometry:
-    """Represent base geometry."""
+    '''
+    this represents BaseGeometry
+    '''
+    
+    def __init_subclass__(cls):
+        bg = BaseGeometry()
+        """Creating an instance for BaseGeometry"""
+        attributes_and_methods = dir(bg)
+        """Getting attributes and methods of instance"""
+        class_attributes_and_methods = dir(BaseGeometry)
 
+        pass
+    
+    def __dir__(self):
+        """ Get the default list of attributes and methods"""
+        default_attrs = super().__dir__()
+    
+
+        """Exclude '__init_subclass__' if it exists in the list"""
+        if '__init_subclass__' in default_attrs:
+            default_attrs.remove('__init_subclass__')
+
+        return default_attrs
     def area(self):
         """Not implemented."""
         raise Exception("area() is not implemented")
+    
+
+
+    #"""Defines a base geometry class BaseGeometry."""
+
+
+#class BaseGeometry:
+    #"""Represent base geometry."""
+
+    #def area(self):
+        #"""Not implemented."""
+        #raise Exception("area() is not implemented")
