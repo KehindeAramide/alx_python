261c028711d46bf864cba430269c32846f88e989
KehindeAramide <kenned2k4@yahoo.com>
2023-09-06 21:48:21 +0100

    Task9 upd

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,3 +1,4 @@
+"""Writting the class Square that inherits from Rectangle"""
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
@@ -18,10 +19,10 @@ class Square(Rectangle):
             y (int): The y-coordinate of the square's position (default is 0).
             id (int): The unique identifier of the square (default is None).
         """
-        # Call the constructor of the Rectangle class and pass the necessary arguments
+        """Call the constructor of the Rectangle class and pass the necessary arguments"""
         super().__init__(size, size, x, y, id)
 
-    # The __str__ method to return a string representation of the Square
+    """The __str__ method to return a string representation of the Square"""
     def __str__(self):
         """
         Return a string representation of the Square.
