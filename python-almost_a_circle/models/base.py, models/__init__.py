34ad103aa685e57ab0fd737fbf5aa737b0349c5a
KehindeAramide <kenned2k4@yahoo.com>
2023-09-06 13:03:04 +0100

    Task0

---
+++ python-almost_a_circle/models/base.py, models/__init__.py
@@ -0,0 +1,11 @@
+class Base:
+    # Private class attribute
+    __nb_objects = 0
+
+    # Class constructor
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
