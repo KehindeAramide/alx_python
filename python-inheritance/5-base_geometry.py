e86829c450f5a7a2eb87c8df3f6084b5e37e57ff
KehindeAramide <kenned2k4@yahoo.com>
2023-09-06 09:43:03 +0100

    Task5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,11 +1,32 @@
-"""Defines a base geometry class BaseGeometry."""
-
-
+"""
+Define an empty class named BaseGeometry
+"""
 class BaseGeometry:
-    """Reprsent base geometry."""
+    '''
+    this represents BaseGeometry
+    '''
+    
+    def __init_subclass__(cls):
+        bg = BaseGeometry()
+        """Creating an instance for BaseGeometry"""
+        attributes_and_methods = dir(bg)
+        """Getting attributes and methods of instance"""
+        class_attributes_and_methods = dir(BaseGeometry)
 
+        pass
+    
+    def __dir__(self):
+        """ Get the default list of attributes and methods"""
+        default_attrs = super().__dir__()
+    
+
+        """Exclude '__init_subclass__' if it exists in the list"""
+        if '__init_subclass__' in default_attrs:
+            default_attrs.remove('__init_subclass__')
+
+        return default_attrs
     def area(self):
-        """Not yet implemented."""
+        """Not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
