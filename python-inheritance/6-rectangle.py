a96ccbe0afaa58157b4f317bc6e6506bbc80334c
KehindeAramide <kenned2k4@yahoo.com>
2023-09-06 11:13:38 +0100

    task6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -7,7 +7,6 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Intialize a new Rectangle."""
-        BaseGeometry = Rectangle()
         attributes_and_methods = dir(Rectangle)
         """Getting attributes and methods of instance"""
         class_attributes_and_methods = dir(Rectangle)
