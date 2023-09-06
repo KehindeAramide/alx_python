fafd7e11b7da94d777e0ca8eff055941b483c688
KehindeAramide <kenned2k4@yahoo.com>
2023-09-06 10:38:46 +0100

    task8

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,15 @@
+"""Writing a class square that inherits from Rectangle(7-rectangle.py)."""
+Rectangle = __import__('7-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializing a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
