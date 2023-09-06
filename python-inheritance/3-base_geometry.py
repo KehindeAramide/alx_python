a44faf4868323cf3fe9decf6f90ea6454f05fb20
KehindeAramide <kenned2k4@yahoo.com>
2023-09-06 09:28:51 +0100

    task3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -7,14 +7,20 @@ class BaseGeometry:
     '''
     
     def __init_subclass__(cls):
+        bg = BaseGeometry()
+        """Creating an instance for BaseGeometry"""
+        attributes_and_methods = dir(bg)
+        """Getting attributes and methods of instance"""
+        class_attributes_and_methods = dir(BaseGeometry)
+
         pass
     
     def __dir__(self):
-        # Get the default list of attributes and methods
+        """ Get the default list of attributes and methods"""
         default_attrs = super().__dir__()
-        default
+    
 
-        # Exclude '__init_subclass__' if it exists in the list
+        """Exclude '__init_subclass__' if it exists in the list"""
         if '__init_subclass__' in default_attrs:
             default_attrs.remove('__init_subclass__')
 
