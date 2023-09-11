2a5e78624d351790f553d4f2b59cbc4e74b27b43
KehindeAramide <kenned2k4@yahoo.com>
2023-09-11 21:43:38 +0100

    Task0 upd

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,2 +1,6 @@
+"""Writing a script that fetches the below url"""
+
 import requests
+"""importing requests"""
 req = requests.get("https://alu-intranet.hbtn.io/status")
+"""this fetches the above url"""
