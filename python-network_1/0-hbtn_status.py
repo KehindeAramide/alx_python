c65b2b8816748a308a1df6eb92d058636a08e3a2
KehindeAramide <kenned2k4@yahoo.com>
2023-09-11 22:22:34 +0100

    task1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,6 +4,3 @@ import requests
 """importing requests"""
 req = requests.get("https://alu-intranet.hbtn.io/status")
 """this fetches the above url"""
-if response.status_code == 200:
-    print(response.text)
-
---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,27 @@
+"""Writing a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header
+"""
+import requests
+import sys
+
+"""Check if the command-line argument is provided"""
+if len(sys.argv) < 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
+
+"""Get the URL from the command-line argument"""
+url = sys.argv[1]
+
+"""Send a GET request to the specified URL"""
+response = requests.get(url)
+
+"""Check if the request was successful (status code 200)"""
+if response.status_code == 200:
+    """Check if the 'X-Request-Id' header is present in the response"""
+    if 'X-Request-Id' in response.headers:
+        """Print the value of the 'X-Request-Id' header
+    """
+        print("X-Request-Id:", response.headers['X-Request-Id'])
+    else:
+        print("X-Request-Id header not found in the response.")
+else:
+    print(f"Request failed with status code {response.status_code}")
