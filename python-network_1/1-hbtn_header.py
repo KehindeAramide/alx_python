7f869de2f786fabb90318447fcb779e75f156aa1
KehindeAramide <kenned2k4@yahoo.com>
2023-09-12 12:59:50 +0100

    task1 upd

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,26 +3,26 @@
 import requests
 import sys
 
-# Check if the command-line argument is provided
+"""Check if the command-line argument is provided"""
 if len(sys.argv) < 2:
     print("Usage: python script.py <URL>")
     sys.exit(1)
 
-# Get the URL from the command-line argument
+"""Get the URL from the command-line argument"""
 url = sys.argv[1]
 
-# Send a GET request to the specified URL
+"""Send a GET request to the specified URL"""
 response = requests.get(url)
 
-# Check if the request was successful (status code 200)
+"""Check if the request was successful (status code 200)"""
 if response.status_code == 200:
-    # Check if the 'X-Request-Id' header is present in the response
+    """Check if the 'X-Request-Id' header is present in the response"""
     if 'X-Request-Id' in response.headers:
-        # Print the value of the 'X-Request-Id' header after stripping whitespace
+        """Print the value of the 'X-Request-Id' header after stripping whitespace"""
         print(response.headers['X-Request-Id'].strip())
     else:
-        # Print a message indicating that the header was not found
+        """Print a message indicating that the header was not found"""
         print("X-Request-Id header not found in the response.")
 else:
-    # Print an error message with the status code
+    """Print an error message with the status code"""
     print(f"Request failed with status code {response.status_code}")
