1e899ef2723a1f7cf1d958f17dab417e78af29ee
KehindeAramide <kenned2k4@yahoo.com>
2023-09-12 17:15:38 +0100

    task0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,15 +3,16 @@
 import requests
 
 """Define the URL to fetch"""
-url = 'https://alu-intranet.hbtn.io/status'
+url = "https://intranet.hbtn.io/status"
 
 """Send a GET request to the URL"""
 response = requests.get(url)
 
 """Check if the request was successful (status code 200)"""
 if response.status_code == 200:
-    """Print the response content"""
-    print("\t" + response.text)
+
+    content_type = type(response.text).__name__
+    content = response.text
+    print(f"Body response:\n\t- type: {content_type}\n\t- content: {content}")
 else:
-    """Print an error message with the status code if the request fails"""
     print(f"Request failed with status code {response.status_code}")
