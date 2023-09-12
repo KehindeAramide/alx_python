abe361d603f7622625b78ddee8ab88c95bc92a4f
KehindeAramide <kenned2k4@yahoo.com>
2023-09-12 16:53:36 +0100

    task1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,26 +3,16 @@
 import requests
 import sys
 
-"""Check if a URL is provided as a command-line argument"""
-if len(sys.argv) < 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
-
-"""Get the URL from the command-line argument"""
+"""Get the URL from the command-line arguments"""
 url = sys.argv[1]
 
 """Send a GET request to the specified URL"""
 response = requests.get(url)
 
-"""Check if the request was successful (status code 200)"""
-if response.status_code == 200:
-    """Check if the 'X-Request-Id' header is present in the response"""
-    if 'X-Request-Id' in response.headers:
-        """Print the value of the 'X-Request-Id' header"""
-        print(response.headers['X-Request-Id'])
-    else:
-        """Print a message when the header is not found"""
-        print("X-Request-Id header not found in the response.")
+"""Check if the 'X-Request-Id' header is present in the response"""
+if 'X-Request-Id' in response.headers:
+    """Print the value of the 'X-Request-Id' header"""
+    print(response.headers['X-Request-Id'])
 else:
-    """Print an error message with the status code if the request fails"""
-    print(f"Request failed with status code {response.status_code}")
+    """Print a message when the header is not found"""
+    print("X-Request-Id header not found in the response.")
