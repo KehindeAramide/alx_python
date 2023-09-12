5fafaeb314715eb3b283895b55f528bc3c82de05
KehindeAramide <kenned2k4@yahoo.com>
2023-09-12 13:24:40 +0100

    task0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,21 +1,17 @@
 import requests
 
-"""end a GET request to the specified URL"""
-response = requests.get("https://alu-intranet.hbtn.io/status")
+"""Send a GET request to the specified URL"""
+response = requests.get("https://intranet.hbtn.io/status")
 
-"""Check if the request was successful (status code 200)"""
+"""Check if the request was successful (status code 200)""""
 if response.status_code == 200:
-    """Check if the 'X-Request-Id' header is present in the response"""
-    if 'X-Request-Id' in response.headers:
-        """Print the value of the 'X-Request-Id' header after stripping whitespace"""
-        print(response.headers['X-Request-Id'].strip())
-    else:
-     """Print None when the header is not found"""
-    print("None")
-"""Print the response body with its type and content"""
-print("Body response:")
-print(f"\t- type: {type(response.text)}")
-print(f"\t- content: {response.text}")
+    """Print the response body with its type and content"""
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
+else:
+    """Print an error message with the status code"""
+    print(f"Request failed with status code {response.status_code}")
 
 
 
