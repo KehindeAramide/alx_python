2d4fed6dc7eac7044ccd989f695072c79bcafc24
KehindeAramide <kenned2k4@yahoo.com>
2023-09-12 17:42:35 +0100

    task5

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -20,3 +20,26 @@ if response.status_code == 200:
     print(f"Your GitHub user ID is: {user_id}")
 else:
     print(f"Failed to fetch user data. Status code: {response.status_code}")
+
+
+import requests
+import sys
+
+# Replace 'YOUR_USERNAME' with your GitHub username and 'YOUR_TOKEN' with your personal access token
+username = ['KehindeAramide']
+token = ['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']
+
+# Create a Basic Authentication header with your username and token
+headers = {'Authorization': 'Basic ' + f'{username}:{token}'.encode('utf-8').hex()}
+
+# Send a GET request to the GitHub API to fetch your user information
+url = 'https://api.github.com/user'
+response = requests.get(url, headers=headers)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    user_data = response.json()
+    user_id = user_data['id']
+    print(f"Your GitHub user ID is: {user_id}")
+else:
+    print(f"Failed to fetch user data. Status code: {response.status_code}")
