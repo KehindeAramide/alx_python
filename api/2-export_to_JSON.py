e7863de29f6944087e0804dc1315aef621a50234
KehindeAramide <kenned2k4@yahoo.com>
2023-10-05 11:45:07 +0100

    task2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,32 +2,33 @@ import json
 import requests
 import sys
 
+def export_to_json(employee_id, tasks):
+    with open(f'{employee_id}.json', 'w') as json_file:
+        json.dump({str(employee_id): tasks}, json_file)
+    print(f'Tasks exported to {employee_id}.json')
+
 def get_employee_info(employee_id):
-    # Getting employee details
+    # Get employee details
     employee_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
     employee_data = employee_response.json()
     username = employee_data['username']
 
-    # Getting employee's TODO list
+    # Get employee's TODO list
     todo_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
     todo_data = todo_response.json()
 
-    # Preparing tasks in JSON format
-    tasks_json = []
+    # Prepare tasks in JSON format
+    tasks = []
     for task in todo_data:
         task_json = {
             'task': task['title'],
             'completed': task['completed'],
             'username': username
         }
-        tasks_json.append(task_json)
+        tasks.append(task_json)
 
-    # Writing tasks to JSON file
-    filename = f'{employee_id}.json'
-    with open(filename, 'w') as jsonfile:
-        json.dump({str(employee_id): tasks_json}, jsonfile)
-
-    print(f'Tasks exported to {filename}')
+    # Export tasks to JSON file
+    export_to_json(employee_id, tasks)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
