53b1922626bfea2d38299ee074b2f68fe70e1fa4
KehindeAramide <kenned2k4@yahoo.com>
2023-10-05 10:37:01 +0100

    task1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+import requests
+import csv
+import sys
+
+def get_employee_info(employee_id):
+    # Get employee details
+    employee_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    employee_data = employee_response.json()
+    user_id = employee_data['id']
+    username = employee_data['username']
+
+    # Get employee's TODO list
+    todo_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+    todo_data = todo_response.json()
+
+    # Write tasks to CSV file
+    filename = f'{user_id}.csv'
+    with open(filename, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+        for task in todo_data:
+            task_completed_status = str(task['completed'])
+            task_title = task['title']
+            csvwriter.writerow([user_id, username, task_completed_status, task_title])
+
+    print(f'Tasks exported to {filename}')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python script.py <employee_id>')
+    else:
+        try:
+            employee_id = int(sys.argv[1])
+            get_employee_info(employee_id)
+        except ValueError:
+            print('Error: Employee ID must be an integer.')
