1e13f2a5e2954132827b5578291d698bf3d449a7
KehindeAramide <kenned2k4@yahoo.com>
2023-10-05 10:56:38 +0100

    task1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,6 +2,10 @@ import csv
 import requests
 import sys
 
+import requests
+import csv
+import sys
+
 def get_employee_info(employee_id):
     # Get employee details
     employee_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
@@ -24,32 +28,12 @@ def get_employee_info(employee_id):
 
     print(f'Tasks exported to {filename}')
 
-def user_info(id):
-    # Get employee details
-    employee_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
-    employee_data = employee_response.json()
-    username = employee_data['username']
-
-    try:
-        with open(str(id) + ".csv", 'r') as f:
-            reader = csv.reader(f)
-            header = next(reader)  # skip header row
-            for row in reader:
-                user_id, csv_username, _, _ = row
-                if user_id == str(id) and csv_username == username:
-                    print("User ID and Username: OK")
-                    return
-        print("User ID or Username: Incorrect")
-    except FileNotFoundError:
-        print(f"File '{id}.csv' not found")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: python script.py <employee_id>')
     else:
         try:
             employee_id = int(sys.argv[1])
             get_employee_info(employee_id)
-            user_info(employee_id)
         except ValueError:
             print('Error: Employee ID must be an integer.')
