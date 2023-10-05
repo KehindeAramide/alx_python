3c4950b25e17158f71e9cf900d5791de26841da9
KehindeAramide <kenned2k4@yahoo.com>
2023-10-05 11:30:11 +0100

    task2 upd

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,19 +1,18 @@
-#exporting data in the JSON format.
 import json
 import requests
 import sys
 
 def get_employee_info(employee_id):
-    # Get employee details
+    # Getting employee details
     employee_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
     employee_data = employee_response.json()
     username = employee_data['username']
 
-    # Get employee's TODO list
+    # Getting employee's TODO list
     todo_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
     todo_data = todo_response.json()
 
-    # Prepare tasks in JSON format
+    # Preparing tasks in JSON format
     tasks_json = []
     for task in todo_data:
         task_json = {
@@ -23,7 +22,7 @@ def get_employee_info(employee_id):
         }
         tasks_json.append(task_json)
 
-    # Write tasks to JSON file
+    # Writing tasks to JSON file
     filename = f'{employee_id}.json'
     with open(filename, 'w') as jsonfile:
         json.dump({str(employee_id): tasks_json}, jsonfile)
