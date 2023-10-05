865f6da5376d29d7a3df4f55efdc890e5a299011
KehindeAramide <kenned2k4@yahoo.com>
2023-10-05 10:00:46 +0100

    Task0 upd

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,7 +13,7 @@ def get_employee_info(employee_id):
 
     # Calculate TODO list progress
     total_tasks = len(todo_data)
-    completed_tasks = sum(1 for task in todo_data if task['completed'])
+    completed_tasks = [task for task in todo_data if task['completed']]
 
     # Print employee TODO list progress
     print(f'Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):')
